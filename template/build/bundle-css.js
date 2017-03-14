c9d684375039e0056e9917325ae7cac34e697ce4
Gopal Shah <simplecooldude1@gmail.com>
2017-03-13 17:09:40 -0700

    fix up css bundler

--- template/build/bundle-css.js
+++ template/build/bundle-css.js
@@ -1,27 +1,18 @@
-var fs = require('fs');
-var path = require('path');
-var CleanCSS = require('clean-css');
+const fs = require('fs');
+const path = require('path');
+const CleanCSS = require('clean-css');
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Get all css files
-var cssDir = path.join(process.cwd(), "css");
-var cssFiles = fs.readdirSync(cssDir);
-var css = "";
+const cssDir = path.join(process.cwd(), "css");
+const cssFiles = fs.readdirSync(cssDir);
+let css = "";
 
 // Concat all files into one big css file
 for(var i = 0; i < cssFiles.length; i++) {
   css += fs.readFileSync(path.join(cssDir, cssFiles[i]));
 }
 
-// Get any previous CSS and make it part of the build
-var builtCSSPath = path.join('dist', 'build.css');
-var previousCSS = "";
-if(fs.existsSync(builtCSSPath)) {
-  previousCSS = fs.readFileSync(builtCSSPath);
-}
-
 // Optimize CSS
-var optimizedCSS = new CleanCSS({
-
-}).minify(css + previousCSS.toString());
-
+const optimizedCSS = new CleanCSS({}).minify(css + css.toString());
 fs.writeFileSync(builtCSSPath, optimizedCSS.styles);
